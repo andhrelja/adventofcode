ad7ad33e6664ccae67f1e14fcd11a019a53ec91f
Andrea Hrelja <andhrelja@hotmail.com>
2019-12-02 23:09:19 +0100

    Day 02

--- 2019/day01/day01.py
+++ 2019/day01/day01.py
@@ -1,11 +1,36 @@
-with open("day01.txt", "r") as f:
-    masses = [int(item) for item in f.readlines()]
+def get_masses():
+    with open("day01.txt", "r") as f:
+        return [int(item) for item in f.readlines()]
+
 
 def get_fuel(masses):
     for mass in masses:
-        yield int(mass / 3) - 2
+        yield mass // 3 - 2
 
 
-if __name__ == '__main__':
+def get_fuel_module(fuel):
+    while fuel > 0:
+        fuel = fuel // 3 - 2
+        if fuel <= 0:
+            yield 0
+        else:
+            yield fuel
+
+
+def get_part1():
+    masses = get_masses()
     fuel_required = get_fuel(masses)
-    print(sum(fuel_required))
+    return sum(fuel_required)
+
+
+def get_part2():
+    masses = get_masses()
+    fuel_required = get_fuel(masses)
+
+    suma = 0
+    for fuel in list(fuel_required):
+        suma += sum(get_fuel_module(fuel), fuel)
+    return suma
+
+print("Part 1:", get_part1())
+print("Part 2:", get_part2())
--- 2019/day02/day02.py
+++ 2019/day02/day02.py
@@ -1,30 +1,38 @@
-opcodes = [1, 2, 99]
+def get_intcode():
+    with open("day02.txt", "r") as f:
+        return [int(x) for x in f.read().split(",")]
 
-with open("day02.txt", "r") as f:
-    numbers = [int(x) for x in f.read().split(",")]
+def process_intcode(numbers):
+    for i in range(0, len(numbers) - 3, 4):
+        first = numbers[i + 1]
+        second = numbers[i + 2]
+        index = numbers[i + 3]
+        if numbers[i] == 1:
+            numbers[index] = numbers[first] + numbers[second]
+        elif numbers[i] == 2:
+            numbers[index] = numbers[first] * numbers[second]
+        else:
+            break
+    return numbers
 
-print(numbers)
 
-for i in range(len(numbers)):
-    if numbers[i] == 1:
-        new_numbers = list()
-        while numbers[i] != 99:
-            new_numbers.append(numbers[i])
-            i += 1
-        suma = 0
-        for j in new_numbers[1:-1]:
-            suma += numbers[j]
-        numbers[new_numbers[-1]] = suma
-    
-    
-    elif numbers[i] == 2:
-        new_numbers = list()
-        while numbers[i] != 99:
-            new_numbers.append(numbers[i])
-            i += 1
-        prod = 1
-        for j in new_numbers[:-1]:
-            prod *= numbers[j]
-        numbers[new_numbers[-1]] = prod
+def get_part1():
+    numbers = get_intcode()
+    numbers[1] = 12
+    numbers[2] = 2
+    return process_intcode(numbers)[0]
 
-print(numbers)
+
+def get_part2():
+    for i in range(100):
+        for j in range(100):
+            numbers = get_intcode()
+            numbers[1] = i
+            numbers[2] = j
+            numbers = process_intcode(numbers)
+            if numbers[0] == 19690720:
+                return (i, j)
+
+
+print("Part 1:", get_part1())
+print("Part 2:", get_part2())
