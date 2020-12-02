874276a57365c102f5b66364f8f7db716e75a120
Andrea Hrelja <andhrelja@hotmail.com>
2020-12-02 13:23:48 +0100

    Day 2

---
+++ 2020/day02/day02.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+cleaned = lambda x: [i.strip() for i in x]
+
+def read_column(filename='day02.txt'):
+    with open(BASE_DIR / filename, 'r') as f:
+        lines = f.readlines()
+        lines = [line.strip() for line in lines]
+        return lines
+
+
+def processed_lines(lines):
+    processed = list()
+    for line in lines:
+        split_line = line.split(":")
+        clean_line = cleaned(split_line)
+
+        dirty_policy = clean_line[0]
+        split_policy = dirty_policy.split(" ")
+
+        policy = cleaned(split_policy)
+        _min, _max = policy[0].split("-")
+        char = policy[1]
+        password = clean_line[1]
+        processed.append({
+            'min_occurance': int(_min),
+            'max_occurance': int(_max),
+            'char': char,
+            'password': password,
+        })
+    return processed
+
+
+def count_occurance(char, password):
+    count = 0
+    for chr in password:
+        if chr == char:
+            count += 1
+    return count
+
+def make_valid(processed_lines):
+    for dictionary in processed_lines:
+        occurances = count_occurance(dictionary['char'], dictionary['password'])
+        if occurances in range(dictionary['min_occurance'], dictionary['max_occurance'] + 1):
+            dictionary['valid'] = True
+        else:
+            dictionary['valid'] = False
+
+def part1(processed_lines):
+    valid_count = 0
+    for dictionary in processed_lines:
+        if dictionary['valid']:
+            valid_count += 1
+    return valid_count
+
+def part2(processed_lines):
+    valid_count = 0
+    for dictionary in processed_lines:
+        i1 = dictionary['min_occurance'] - 1
+        i2 = dictionary['max_occurance'] - 1
+        if dictionary['password'][i1] == dictionary['char'] and dictionary['password'][i2] != dictionary['char'] \
+            or dictionary['password'][i1] != dictionary['char'] and dictionary['password'][i2] == dictionary['char']:
+            valid_count += 1
+    return valid_count
+
+if __name__ == '__main__':
+    lines = read_column()
+    processed = processed_lines(lines)
+    
+    make_valid(processed)
+
+    result1 = part1(processed)
+    print("Day 2, part 1:", result1)
+
+    result2 = part2(processed)
+    print("Day 2, part 2:", result2)
+    
--- readme.md
+++ readme.md
@@ -2,12 +2,13 @@
   
 | Day | Name | Link |
 |---|---|---|
-| Day 1 | The Tyranny of the Rocket Equation | [2019/day01/](https://github.com/andhrelja/adventofcode/tree/master/2019/day01) |
-| Day 2 | 1202 Program Alarm | [2019/day02/](https://github.com/andhrelja/adventofcode/tree/master/2019/day02) |
-| Day 3 | Crossed Wires | [2019/day03/](https://github.com/andhrelja/adventofcode/tree/master/2019/day03) |
+| [Day 1](https://adventofcode.com/2019/day/1) | The Tyranny of the Rocket Equation | [2019/day01/](https://github.com/andhrelja/adventofcode/tree/master/2019/day01) |
+| [Day 2](https://adventofcode.com/2019/day/2) | 1202 Program Alarm | [2019/day02/](https://github.com/andhrelja/adventofcode/tree/master/2019/day02) |
+| [Day 3](https://adventofcode.com/2019/day/3) | Crossed Wires | [2019/day03/](https://github.com/andhrelja/adventofcode/tree/master/2019/day03) |
 
 # Advent Of Code 2020 #
   
 | Day | Name | Link |
 |---|---|---|
-| Day 1 | Report Repair | [2020/day01/](https://github.com/andhrelja/adventofcode/tree/master/2020/day01) |
+| [Day 1](https://adventofcode.com/2020/day/1) | Report Repair | [2020/day01/](https://github.com/andhrelja/adventofcode/tree/master/2020/day01) |
+| [Day 2](https://adventofcode.com/2020/day/2) | Password Philosophy | [2020/day01/](https://github.com/andhrelja/adventofcode/tree/master/2020/day01) |
