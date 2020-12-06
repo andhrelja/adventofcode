06026d1c89795663363287a25d8923da10c9a43d
Andrea Hrelja <andhrelja@hotmail.com>
2020-12-06 18:42:58 +0100

    Day 6, part 2

--- 2020/day06.py
+++ 2020/day06.py
@@ -1,3 +1,4 @@
+from itertools import count
 from utils import file_to_list
 
 # Incomplete
@@ -10,26 +11,27 @@ def part1(lines):
         sum_all += len(distinct_line)
     return sum_all
 
-def count_occurances(string, char):
-    count = 0
-    for ch in string:
-        if ch == char:
-            count += 1
-    return count
-
 def part2(lines):
     sum_all = 0
     for group in lines:
-        members_num = count_occurances(group, '\n')
-        group = group.replace('\n', '')
-        distinct_line = list(set(group))
-        if len(distinct_line) < members_num:
-            sum_all += len(distinct_line)
+        members_answers = group.split('\n')
+        members_num = len(members_answers)
+        if members_num == 1:
+            sum_all += len(members_answers[0])
+        else:
+            first_answer = members_answers[0]
+            for char in first_answer:
+                count_occurances = 1
+                for answer in members_answers[1:]:
+                    if char in answer:
+                        count_occurances += 1
+                if count_occurances == members_num:
+                    sum_all += 1
     return sum_all
 
 
 if __name__ == '__main__':
-    lines = file_to_list('day06.txt', test=True, sep='\n\n')
+    lines = file_to_list('day06.txt', test=False, sep='\n\n')
 
     result1 = part1(lines)
     print("Day 6, part 1:", result1)
