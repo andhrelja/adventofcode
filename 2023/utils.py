8ffbb269eaa869749a2693efb85dba8f7fca6656
andhrelja <andhrelja@hotmail.com>
2023-12-04 19:58:04 +0100

    perf: update utils

--- 2023/utils.py
+++ 2023/utils.py
@@ -2,9 +2,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-stripped = lambda x: [i.strip() for i in x]
-
-def file_to_list(filename, test=False, sep=None, cast=None, strip=True):
+def file_to_list(filename, test=False, sep=None, strip=True, _map=None):
     if test:
         filepath = BASE_DIR / 'test_inputs' / filename
     else:
@@ -16,10 +14,11 @@ def file_to_list(filename, test=False, sep=None, cast=None, strip=True):
         else:
             lines = f.read()
             lines = lines.split(sep)
-        if strip:
-            lines = stripped(lines)
         
-        if cast:
-            return list(map(cast, lines))
+        if strip:
+            lines = map(str.strip, lines)
+        
+        if _map:
+            return map(_map, lines)
         else:
             return lines
